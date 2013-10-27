d90ced3e0db2d9c3e769d62fec7257b8321497e2
nikolas <niko@nachtproduktion.de>
2013-10-28 00:44:53 +0100

    konstanten datei

---
+++ src/constants.h
@@ -0,0 +1,14 @@
+//
+//  constants.h
+//  BewegungsStudien
+//
+//  Created by Nikolas on 28.10.13.
+//
+//
+
+#ifndef BewegungsStudien_constants_h
+#define BewegungsStudien_constants_h
+
+
+
+#endif
